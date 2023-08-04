9a1859c9f2c5d6166746e86bacf7dff8e17ad41c
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-08-04 12:33:08 +0530

    render pet profiles with render()

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Remember to import the data and Dog class!
 import Dog from "./Dog.js";
 import dogs from "./data.js";
+
+let currentIndex = 0;
+let currentDog = new Dog(dogs[currentIndex]);
+
+function render() {
+  document.getElementById("card").innerHTML = currentDog.getDogHtml();
+}
+
+
+render()
