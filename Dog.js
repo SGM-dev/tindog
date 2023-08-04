e39cd1c5d14aac24208831a0c64d6258dfb494b7
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-08-04 11:36:17 +0530

    setup Dog constructor

--- Dog.js
+++ Dog.js
@@ -1,1 +1,31 @@
 // Create the Dog class here
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio } = this;
+    return `
+    <div class="profile">
+        <img
+            id="like-badge"
+            class="badge like hidden"
+            src="/images/badge-like.png"
+            alt="like badge"
+         />
+        <img
+            id="nope-badge"
+            class="badge nope hidden"
+            src="/images/badge-nope.png"
+            alt="nope badge"
+        />
+        <div class="profile-content">
+            <h3>${name}, ${age}</h3>
+            <span>${bio}</span>
+        </div>
+        <img src="${avatar}" alt="Pet's Picture" />
+    </div>`;
+  }
+}
