c18f86f7d06c5f4423429512ae8ca810c65a398c
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-08-04 12:59:41 +0530

    add event listener for buttons

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ import dogs from "./data.js";
 let currentIndex = 0;
 let currentDog = new Dog(dogs[currentIndex]);
 
+document.addEventListener("click", function (e) {
+  if (e.target.id == "like-btn") {
+    console.log(e.target);
+  } else if (e.target.id == "reject-btn") {
+    console.log(e.target);
+  }
+});
+
 function render() {
   document.getElementById("card").innerHTML = currentDog.getDogHtml();
 }
 
-
-render()
+render();
