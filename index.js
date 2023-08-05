953b76f75012675cd6a20c2bd2d90ac47d0258c0
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-08-05 08:52:14 +0530

    get next pet for rendering

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 import Dog from "./Dog.js";
 import dogs from "./data.js";
 
-let currentIndex = 0;
-let currentDog = new Dog(dogs[currentIndex]);
+let currentDog = new Dog(dogs.shift());
 
 document.addEventListener("click", function (e) {
   if (e.target.id == "like-btn") {
-    console.log(e.target);
+    getNewDog();
   } else if (e.target.id == "reject-btn") {
-    console.log(e.target);
+    getNewDog();
   }
 });
+
+function getNewDog() {
+  const nextDogData = dogs.shift();
+  currentDog = new Dog(nextDogData);
+  render();
+}
 
 function render() {
   document.getElementById("card").innerHTML = currentDog.getDogHtml();
